1439561937fc77263bca4dce6aab2b4e1a30143c
ranurajput <rrajput9760@gmail.com>
2022-05-24 20:47:34 +0530

    Initial commit

---
+++ src/main/java/com/knoldus/messagesModule/controller/Valid.java
@@ -0,0 +1,4 @@
+package com.knoldus.messagesModule.controller;
+
+public @interface Valid {
+}
---
+++ src/test/java/com/knoldus/messagesModule/MessagesModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoldus.messagesModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
